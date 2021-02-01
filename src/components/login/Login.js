3b8bcda20007459277360098a8f62fdf4c593214
mariogr93 <mariogr93@gmail.com>
2021-02-01 01:58:13 -0400

    Created new component Login and added basic style and functionality to create a new id using uuid library

---
+++ src/components/login/Login.js
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { v4 as uuidV4 } from "uuid";
+
+function Login({ onIdSubmit }) {
+  const idRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    onIdSubmit(idRef.current.value);
+  };
+
+  const createNewId = () => {
+    console.log("aqui");
+    onIdSubmit(uuidV4());
+  };
+
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: "100vh" }}
+    >
+      <Form onSubmit={submitHandler} className="w-100">
+        <Form.Group>
+          <Form.Label className="text-center w-100">Enter your ID</Form.Label>
+          <Form.Control type="text" ref={idRef}></Form.Control>
+        </Form.Group>
+        <Button type="submit" className="mr-2">
+          Login
+        </Button>
+        <Button onClick={createNewId} variant="secondary">
+          Register
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default Login;
