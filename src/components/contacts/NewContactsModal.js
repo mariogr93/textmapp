68944c2373d187c22571218c5863051065f8367a
mariogr93 <mariogr93@gmail.com>
2021-02-03 01:01:31 -0400

    Created newContactModal component to render a modal to create new contacts

---
+++ src/components/contacts/NewContactsModal.js
@@ -0,0 +1,41 @@
+import React, { useRef } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+
+function NewContactsModal({ closeModal }) {
+  const idRef = useRef();
+  const nameRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = idRef.current.value;
+    const name = nameRef.current.value;
+    //addContactHandler(id, name)
+    closeModal();
+  }
+
+  return (
+    <React.Fragment>
+      <Modal.Header closeButton>Create Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>ID</Form.Label>
+            <Form.Control type="text" ref={idRef} required></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} required></Form.Control>
+          </Form.Group>
+          <Form.Group className="float-right">
+            <Button onClick={closeModal} variant="danger" className="mr-2 ">
+              Close
+            </Button>
+            <Button type="submit">Add contact</Button>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </React.Fragment>
+  );
+}
+
+export default NewContactsModal;
