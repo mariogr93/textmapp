2a0f58bae608889f635e715911681fa69fdc57ba
mariogr93 <mariogr93@gmail.com>
2021-02-02 22:44:57 -0400

    Added Dashboard component and Local Storage to handle the registration of the ID

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import UseLocalStorage from "./components/hooks/UseLocalStorage";
 
 function App() {
-  const [id, setId] = useState();
-  return (
-    <React.Fragment>
-      {id}
-      <Login onIdSubmit={setId} />
-    </React.Fragment>
-  );
+  const [id, setId] = UseLocalStorage("id");
+
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
 }
 
 export default App;
