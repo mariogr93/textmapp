521adac3c657d85d885b64dee992e01412977072
mariogr93 <mariogr93@gmail.com>
2021-02-04 14:14:47 -0400

    Changed function names to useContactsContext and  addContactHandler

--- src/contexts/ContactsProvider.js
+++ src/contexts/ContactsProvider.js
@@ -3,20 +3,20 @@ import useLocalStorage from "../components/hooks/UseLocalStorage";
 
 const ContactsContext = React.createContext();
 
-export function useContacts() {
+export function useContactsContext() {
   return useContext(ContactsContext);
 }
 
 export function ContactsProvider({ children }) {
   const [contacts, setContacts] = useLocalStorage("contacts", []);
 
-  function createContact(id, name) {
+  function addContactHandler(id, name) {
     setContacts((prevContacts) => {
-      return [...prevContacts, , { id, name }];
+      return [...prevContacts, { id, name }];
     });
   }
   return (
-    <ContactsContext.Provider value={{ contacts, createContact }}>
+    <ContactsContext.Provider value={{ contacts, addContactHandler }}>
       {children}
     </ContactsContext.Provider>
   );
