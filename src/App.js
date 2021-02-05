5c656fef3c79406f1084cdb979742df56d98f850
mariogr93 <mariogr93@gmail.com>
2021-02-05 00:23:34 -0400

    Added the new ConversationsProvider

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import useLocalStorage from "./components/hooks/UseLocalStorage";
 import { ContactsProvider } from "./contexts/ContactsProvider";
+import { ConversationsProvider } from "./contexts/ConversationsProvider";
 function App() {
   const [id, setId] = useLocalStorage("id");
 
   const dashboard = (
     <ContactsProvider>
-      <Dashboard id={id} />
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
   );
   return id ? dashboard : <Login onIdSubmit={setId} />;
