ee5ede1bc882d5ce0220b5d2395f9e3883580a86
mariogr93 <mariogr93@gmail.com>
2021-02-09 23:18:53 -0400

    Added new conversationFormater, commented old code just in case

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -26,8 +26,9 @@
 //   );
 // }
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import useLocalStorage from "../components/hooks/UseLocalStorage";
+import { useContactsContext } from "./ContactsProvider";
 
 const ConversationsContext = React.createContext();
 
@@ -40,6 +41,8 @@ export function ConversationsProvider({ id, children }) {
     "conversations",
     []
   );
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+  const { contacts } = useContactsContext();
 
   function createConversation(recipients) {
     setConversations((prevConversations) => {
@@ -47,7 +50,33 @@ export function ConversationsProvider({ id, children }) {
     });
   }
 
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient;
+      });
+      const name = (contact && contact.name) || recipient;
+      return { id: recipient, name };
+    });
+
+    const messages = conversation.messages.map((message) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === message.sender;
+      });
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, messages, recipients, selected };
+  });
+
   const value = {
+    conversations: formattedConversations,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+
+    selectConversationIndex: setSelectedConversationIndex,
     createConversation,
   };
 
